de18f3cffd8dd091b68e1913187be9f6da973d2f
jeroen weij <jeroenweij88@gmail.com>
2024-04-26 22:01:12 +0200

    message

--- fileupload.js
+++ fileupload.js
@@ -1,4 +1,3 @@
-import formidable from '/usr/local/lib/node_modules/formidable/src/index.js';
 import BasicPage from './basicpage.js';
 
 export default class FileUpload extends BasicPage {
@@ -8,28 +7,73 @@ export default class FileUpload extends BasicPage {
 
     async handle(request, response) {
         if (request.method === 'POST') {
-            console.log("Posting!");
-            const form = formidable({});
-            let fields;
-            let files;
-            try {
-                form.on('fileBegin', function (name, file){
-                    file.filepath = __dirname + '/uploads/' + file.originalFilename;
+            let body = await this.getPageData(request);
+
+            let data = body.split("\n").map(function (item) {
+                return item.trim();
+            });
+
+            let typechecked = false;
+            let names = null;
+            let numbers = null;
+            let project = "0";
+            let activity = "0";
+            const columnoffset = 4;
+            for (const line of data) {
+                if (!typechecked) {
+                    if (line === 'Content-Type: text/csv')
+                        typechecked = true;
+                    continue;
+                }
+                if (line.length === 0)
+                    continue;
+
+                let vars = line.split(",").map(function (item) {
+                    return item.trim();
                 });
+                if (vars.length < 5)
+                    continue;
+                if (vars[columnoffset] === "Medewerker")
+                    continue;
+                if (names === null) {
+                    names = vars.slice(columnoffset);
+                    console.log(names);
+                    continue;
+                }
+                if (numbers === null) {
+                    numbers = vars.slice(columnoffset);
+                    console.log(numbers);
+                    continue;
+                }
                 
-                form.on('file', function (name, file){
-                    console.log('Uploaded ' + file.name);
-                });
+                if (vars[0].length > 0)
+                    project = vars[0]
+                if (vars[2].length > 0)
+                    activity = vars[2]
 
-                [fields, files] = await form.parse(request);
-
-            } catch (err) {
-                console.error(err);
-                response.write('ERROR');
+                console.log(`project ${project} act ${activity} ` + line);
             }
-            console.log('data:')
-            console.log(JSON.stringify({ fields, files }, null, 2));
+
         }
         super.handle(request, response);
+    }
+
+    getPageData(request) {
+        return new Promise((resolve, reject) => {
+            var body = '';
+
+            request.on('data', function (data) {
+                body += data;
+
+                // Too much POST data, kill the connection!
+                // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+                if (body.length > 1e6)
+                    request.connection.destroy();
+            });
+
+            request.on('end', function () {
+                resolve(body);
+            });
+        });
     }
 }
